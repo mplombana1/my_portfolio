40cbf589f2bfe8f83a7a6009b61023f2e52b9d83
Matthew Lombana <mplombana@gmail.com>
2017-10-03 14:43:16 -0400

    trying to get pics to work on heroku

--- app/assets/javascripts/smoothscroll.js
+++ app/assets/javascripts/smoothscroll.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-  $('.slideButton').click(function(e) {
+  $('.slideButton').on('click', '#smooth', function(e) {
 
-    var linkHref = $(this).attr('href');
+    var linkHref = $(this)
 
     $('html, body').animate({
       scrollTop: $(linkHref).offset().top
+
+
+
     });
+
+
+
+
 
     e.preventDefault();
   });
